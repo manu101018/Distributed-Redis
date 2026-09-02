092a751c5efe765d34d0bf2e17388e41c9c86a4c
MANJEET SINGH <manjeetsinghmzn2002@gmail.com>
2026-09-02 22:12:49 +0530

    step 1: bare TCP echo server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,25 @@
-console.log('hello');
+import * as net from "net";
+
+const PORT = 8000;
+const HOST = "0.0.0.0";
+
+const server = net.createServer((connection) => {
+    console.log('client conncted');
+
+    connection.on('data', (chunks: Buffer)=>{
+        console.log("data received", chunks.toString());
+        connection.write(chunks);
+    })
+
+    connection.on('close',() =>{
+        console.log("client disconnected");
+    })
+
+    connection.on('error',(error) =>{
+        console.log("client error", error);
+    })
+})
+
+server.listen(PORT, HOST, ()=>{
+    console.log(`Server is running on ${HOST}:${PORT}`);
+})
